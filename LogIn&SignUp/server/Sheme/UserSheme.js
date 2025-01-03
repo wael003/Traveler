75365d17cd20b61a045f85a5d00f55f9e8c23794
wael003 <93313945+wael003@users.noreply.github.com>
2025-01-03 22:39:07 +0300

    Update UserSheme.js

--- LogIn&SignUp/server/Sheme/UserSheme.js
+++ LogIn&SignUp/server/Sheme/UserSheme.js
@@ -4,7 +4,11 @@ const UserSchema = mongoose.Schema({
     UserName: { type: String, required: true},
     Password: { type: String, required: true},
     Email: { type: String, required: true },
-    VerificationCode: { type: Number, required: true }
+    Address :{type : String ,required: false },
+    Phone : {type : String , required: false},
+    Bio : {type : String , required: false},
+    ProfilePic : {type : String , required: false},
+    VerificationCode: { type: Number, required: false }
 });
 
 module.exports = mongoose.model('User', UserSchema);
